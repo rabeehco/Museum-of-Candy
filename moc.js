4ea5a503a1eae7188a375674499c91c66bae07e3
rabeeh47 <rabeehebrahim48@gmail.com>
2022-05-08 18:47:02 +0530

    removed the animation transition

--- moc.js
+++ moc.js
@@ -1,12 +1,12 @@
 var navbar = document.getElementById('navbar-main')
 // history.scrollRestoration = "manual"
-window.addEventListener('scroll', (e) => {  
+// window.addEventListener('scroll', (e) => {  
    
-    if(window.scrollY==0){
-        navbar.style.backgroundColor = 'transparent'
-        navbar.style.transition = '1s'
-    } else {
+//     if(window.scrollY==0){
+//         navbar.style.backgroundColor = 'transparent'
+//         navbar.style.transition = '1s'
+//     } else {
         navbar.style.backgroundColor = '#a486bd'
-        navbar.style.transition = '500ms'
-    }  
-  })
+        // navbar.style.transition = '500ms'
+//     }  
+//   })
